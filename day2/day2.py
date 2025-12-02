e1a082511e67dacfe7fe521208b50b844817c3fb
Dillon Bordeleau <dillonbordeleau@gmail.com>
2025-12-02 18:57:11 -0500

    Day 2 part 2 solved

--- day2/day2.py
+++ day2/day2.py
@@ -6,7 +6,7 @@ def get_invalid_ids(ranges):
         end = int(end)
 
         # Anything with a leading 0 is an invalid id
-        # Any number comprised of a repeating sequence of digits e.g: 4567845678, 55
+        # Any number comprised of a repeating sequence of digits e.g: 4567845678, 55, 111, 333333
         for num in range(start, end+1):
             num_str = str(num)
             
@@ -15,15 +15,12 @@ def get_invalid_ids(ranges):
                 invalids.append(num)
                 continue
 
-            # check if comprised only of a sequence of digits repeated twice
-            if len(num_str) % 2 != 0:
-                continue
-            
-            mid = len(num_str) // 2
-            left = num_str[:mid]
-            right = num_str[mid:]
+            # check if comprised only of repeating digits
+            double_num_str = num_str + num_str # double the string so 789789 becomes 789789789789
+            double_num_str = double_num_str[1:-1] # remove first and last digit
 
-            if left == right:
+            # if the original string is still in the doubled string after removing first and last digits, its comprised of only repeating digit sequence
+            if num_str in double_num_str:
                 invalids.append(num)
 
     return invalids
@@ -32,6 +29,7 @@ def main():
     with open("../input/day2_input") as f:
         input_string = f.read()
         ranges = input_string.split(",")
+        print(ranges)
         invalid_ids = get_invalid_ids(ranges)
         answer = sum(invalid_ids)
         print(answer)
