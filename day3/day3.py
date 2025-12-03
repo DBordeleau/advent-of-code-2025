9eabef3fd408e4e09c39c3220c8a6e630dc0a8dc
Dillon Bordeleau <dillonbordeleau@gmail.com>
2025-12-03 11:42:48 -0500

    Solved day 3 part 1

---
+++ day3/day3.py
@@ -0,0 +1,37 @@
+def get_max_joltages(battery_banks):
+    max_joltages = []
+    for battery_bank in battery_banks:
+        battery_bank = battery_bank.strip()
+        max_seen = "0"
+        max_seen_idx = 0
+        first_digit = 0
+        second_digit = 0
+        broke_early = False
+
+        # find the largest digit before the last, if we find 9 break immediately since 9 is the largest possible digit
+        for i in range(len(battery_bank) - 1):
+            if battery_bank[i] == '9':
+                first_digit = '9'
+                second_digit = max(battery_bank[i+1:])
+                broke_early = True
+                break
+            if battery_bank[i] > max_seen:
+                max_seen = battery_bank[i]
+                max_seen_idx = i
+            
+        if not broke_early:
+            first_digit = max_seen
+            second_digit = max(battery_bank[max_seen_idx + 1:])
+            
+        max_joltages.append(int(first_digit + second_digit))
+    return max_joltages
+
+def main():
+    with open("../input/day3_input") as f:
+        battery_banks = f.readlines()
+    
+    max_joltages = get_max_joltages(battery_banks)
+    print(sum(max_joltages))
+
+if __name__ == "__main__":
+    main()
