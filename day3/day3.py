09cfe74a87b0f0c86d60622d14a41d36939d2bba
Dillon Bordeleau <dillonbordeleau@gmail.com>
2025-12-03 14:13:23 -0500

    solved day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -26,11 +26,44 @@ def get_max_joltages(battery_banks):
         max_joltages.append(int(first_digit + second_digit))
     return max_joltages
 
+def get_max_joltages_part_2(battery_banks):
+    max_joltages = []
+    for battery_bank in battery_banks:
+        digits = []
+        battery_bank = battery_bank.strip()
+
+        next_idx = 0
+
+        # find 12 digits
+        while len(digits) < 12:
+            max_seen_digit = '0'
+            broke_early = False
+            digits_to_find = 11 - len(digits)
+            for i in range(next_idx, len(battery_bank) - digits_to_find):
+                if battery_bank[i] == '9':
+                    digits.append('9')
+                    next_idx = i + 1
+                    broke_early = True
+                    break
+
+                if battery_bank[i] > max_seen_digit:
+                    max_seen_digit = battery_bank[i]
+                    next_idx = i + 1
+            
+            if not broke_early:
+                digits.append(max_seen_digit)
+        
+        # print(battery_bank)
+        # print(digits)
+        max_joltages.append(int(''.join(digits)))
+    
+    return max_joltages
+
 def main():
     with open("../input/day3_input") as f:
         battery_banks = f.readlines()
     
-    max_joltages = get_max_joltages(battery_banks)
+    max_joltages = get_max_joltages_part_2(battery_banks)
     print(sum(max_joltages))
 
 if __name__ == "__main__":
